c358b00d54b4457a513845ee72219ae04c035db5
rockvivek <vinodkumarshrivastawa@gmail.com>
2024-03-07 00:14:30 +0530

    First Commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Vivek Shrivastava on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
